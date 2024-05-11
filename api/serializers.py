8e2984ca3aaf7fcda8d5fe62fc28c1d64d2007f1
johnibek <jonibekhamroqulov2004@gmail.com>
2024-05-12 00:10:50 +0500

    category

--- api/serializers.py
+++ api/serializers.py
@@ -9,12 +9,13 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ToDoSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
-    category = serializers.StringRelatedField()
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
     datetime = serializers.SerializerMethodField(method_name='get_datetime')
 
     class Meta:
         model = Todo
-        fields = ['id', 'user', 'category', 'body', 'done', 'datetime']
+        fields = ['id', 'user', 'body', 'done', 'datetime', 'category', 'category_id']
 
     def get_datetime(self, todo_item: Todo):
         return todo_item.datetime.strftime("%d-%m-%Y %H:%M:%S %p")
