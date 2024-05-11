988a979cf4a80b5204f8dc6620fa22ce7e0667df
johnibek <jonibekhamroqulov2004@gmail.com>
2024-05-11 22:55:03 +0500

    token authentication

--- todo/settings.py
+++ todo/settings.py
@@ -46,6 +46,11 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
